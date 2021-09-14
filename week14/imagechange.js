9b35e976da160f61ac0894b4c7ea7d444e543982
tram3328 <sashaignateva94@mail.ru>
2021-09-14 23:36:24 +0300

    task 3 added

---
+++ week14/imagechange.js
@@ -0,0 +1,14 @@
+function changeMe1() {
+    let img1 = document.getElementById('img1');
+    img1.src = "https://www.touristsecrets.com/wp-content/uploads/2019/06/Feature-Photo_Porto.jpg"
+}
+
+function changeMe2() {
+    let img1 = document.getElementById('img1');
+    img1.src = "https://lp-cms-production.imgix.net/image_browser/Porto-sunset.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2"
+}
+
+function changeMe3() {
+    let img1 = document.getElementById('img1');
+    img1.src = "https://lp-cms-production.imgix.net/image_browser/Cl%C3%A9rigos-Tower-Porto.jpg"
+}
