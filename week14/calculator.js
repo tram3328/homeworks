8abbe5a2828808a13588fe01c11ad561ee540331
tram3328 <sashaignateva94@mail.ru>
2021-10-03 21:39:19 +0300

    calculator added

---
+++ week14/calculator.js
@@ -0,0 +1,35 @@
+//сложение 
+function sum() {
+  let n1 = document.getElementById('num1').value;
+  let n2 = document.getElementById('num2').value;
+ let result = +n1 + +n2;
+ document.getElementById('out').innerHTML = result;
+}
+
+//вычитание
+function substract() {
+  let n1 = document.getElementById('num1').value;
+ let n2 = document.getElementById('num2').value;
+
+ let result = +n1 - +n2;
+ document.getElementById('out').innerHTML = result;
+}
+
+//умножнение
+
+function multiplication() {
+  let n1 = document.getElementById('num1').value;
+  let n2 = document.getElementById('num2').value;
+
+  let result = +n1 * +n2;
+  document.getElementById('out').innerHTML = result;
+}
+
+//деление
+function division() {
+ let n1 = document.getElementById('num1').value;
+ let n2 = document.getElementById('num2').value;
+
+ let result = +n1 / +n2;
+ document.getElementById('out').innerHTML = result;
+}
